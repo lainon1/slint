f89ff65643f028b4baf22b478aa173e3d7cfe3a7
Andreas Monitzer <andreas.monitzer@slint.dev>
2026-04-17 10:21:34 +0200

    Simplified LspToPreview by always using the SwitchableLspToPreview type as the root.

--- tools/lsp/common.rs
+++ tools/lsp/common.rs
@@ -34,7 +34,7 @@ use crate::wasm_prelude::*;
 /// ignore a node for code analysis purposes.
 pub const NODE_IGNORE_COMMENT: &str = "@lsp:ignore-node";
 
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub enum PreviewTarget {
     #[allow(dead_code)]
     ChildProcess,
@@ -45,9 +45,8 @@ pub enum PreviewTarget {
 }
 
 #[allow(dead_code)]
-pub trait LspToPreview {
+pub trait LspToPreview: std::any::Any {
     fn send(&self, message: &LspToPreviewMessage);
-    fn set_preview_target(&self, target: PreviewTarget) -> Result<()>;
     fn preview_target(&self) -> PreviewTarget;
     fn shutdown<'a>(&'a self) -> std::pin::Pin<Box<dyn std::future::Future<Output = ()> + 'a>> {
         Box::pin(async {})
@@ -63,10 +62,6 @@ impl LspToPreview for DummyLspToPreview {
 
     fn preview_target(&self) -> PreviewTarget {
         PreviewTarget::Dummy
-    }
-
-    fn set_preview_target(&self, _: PreviewTarget) -> Result<()> {
-        Err("Can not change the preview target".into())
     }
 }
 
